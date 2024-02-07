5138662b9ccd1820e4f79e2036155b8d84f2184a
Zakhar Litvinchuk <zakhlit@gmail.com>
2024-02-08 01:00:38 +0200

    Simple verstka

--- MyRedditProject/ViewController.swift
+++ MyRedditProject/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shareButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        shareButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        shareButton.titleLabel?.minimumScaleFactor = 0.5
+
     }
 
 
