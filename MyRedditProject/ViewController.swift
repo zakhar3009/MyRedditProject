6589ed84e04d3c39a5d17b8d24b51c405411070d
Zakhar Litvinchuk <zakhlit@gmail.com>
2024-02-07 19:17:44 +0200

    First commit

---
+++ MyRedditProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyRedditProject
+//
+//  Created by Zakhar Litvinchuk on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
